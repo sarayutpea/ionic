a1d63a0f5fba33044aefc5a69a242535f41cf306
pae <pae.sarayut@megazy.com>
2017-06-26 11:28:42 +0700

    subscription get error

--- src/pages/courses/courses.ts
+++ src/pages/courses/courses.ts
@@ -4,6 +4,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Item } from '../../models/item';
 import { CoursesProvider } from '../../providers/courses/courses';
 
+import { Subscription } from 'rxjs/Subscription';
 /**
  * Generated class for the CoursesPage page.
  *
@@ -17,13 +18,27 @@ import { CoursesProvider } from '../../providers/courses/courses';
 })
 export class CoursesPage {
   courses:Item[];
+  subscription:Subscription;
+  errorMessage:string;
 
   constructor(private navCtrl: NavController, public navParams: NavParams, private coursesProvider:CoursesProvider) {
     
   }
 
+  getCourses(){
+    return this.coursesProvider.getCourses().subscribe(
+                                              (res) => this.courses = res, 
+                                              (error) => this.errorMessage = <any>error,
+                                              () => console.log('Completed')
+                                            );
+  }
+
   ionViewWillEnter() {
-    this.coursesProvider.getCourses().subscribe((res) => this.courses = res);
+    this.subscription = this.getCourses(); 
+  }
+
+  ionViewWillLeave() {
+    this.subscription.unsubscribe();
   }
 
 }
